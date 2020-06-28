e755052ce2badbf5b234bc979ee2db2d8d1ad30b
kamalkumar <kamal.kumar@cmlmediasoft.com>
2020-06-29 00:21:20 +0530

    added new class in

---
+++ FloatingUITextField/FloatingUITextField/FloatingTextFieldConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Foundation;
+using UIKit;
+namespace FloatingUITextField.KK
+{
+    public static class FloatingTextFieldConfig
+    {
+
+        public static UIFont DEFAULT_TEXTFIELD_FONT;
+        public static UIFont FLOATINGLABEL_FONT;
+        public static UIColor DEFAULT_TEXTFIELD_COLOR;
+        public static UIColor FLOATING_THEME_COLOR;
+        public static bool CLEAR_BUTTON_ENABLED;
+
+
+    }
+}
